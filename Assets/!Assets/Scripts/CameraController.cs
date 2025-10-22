6eb878f5d9bcbfa24ef824d76a89f360c07461f0
秀宇的逆风小径 <zhangxiuyu@xiuyudenifengxiaojingdeMacBook-Pro.local>
2025-10-22 16:54:11 +0100

    add character movement and camera following

---
+++ Assets/!Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    private float xBound = 3;
+    private float yBound = 5;
+    private float cameraSmoothSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Mathf.Abs(player.position.x - transform.position.x) > xBound)
+            {
+            Vector3 cameraTarget = new Vector3(player.position.x - Mathf.Sign(player.position.x - transform.position.x) * xBound, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, cameraTarget, 5f);
+            }
+
+
+    }
+}
---
+++ Assets/!Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float movementSpeed = 2f;
+    private float deadzone = 0.01f;
+    private float rotationTimer;
+    [SerializeField]private GameObject avatar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.position = new Vector3(transform.position.x + movementSpeed*Time.deltaTime, transform.position.y, transform.position.z);
+            avatar.transform.rotation = Quaternion.Euler(0, 90, 0);
+            avatar.GetComponent<Animator>().SetBool("isWalking",true);
+            rotationTimer = 0;
+        }
+
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.position = new Vector3(transform.position.x - movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+            avatar.transform.rotation = Quaternion.Euler(0, -90, 0);
+            avatar.GetComponent<Animator>().SetBool("isWalking", true);
+            rotationTimer = 0;
+        }
+
+        else if(Input.GetAxis("Horizontal") == 0)
+        {
+            rotationTimer = rotationTimer + Time.deltaTime;
+            if (rotationTimer > deadzone)
+            {
+                avatar.GetComponent<Animator>().SetBool("isWalking", false);
+
+            }
+            
+        }
+
+    }
+}
